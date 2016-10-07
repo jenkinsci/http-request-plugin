dc62f3925231b74d49b6f763b39851377f0eeea5
drighetto <drighetto@excellium-services.com>
2016-10-07 22:58:25 +0200

    Add supports for HTML FORM MimeType

--- src/main/java/jenkins/plugins/http_request/MimeType.java
+++ src/main/java/jenkins/plugins/http_request/MimeType.java
@@ -7,30 +7,25 @@ import hudson.util.ListBoxModel;
  */
 public enum MimeType {
 
-    NOT_SET(""),
-    TEXT_HTML("text/html"),
-    TEXT_PLAIN("text/plain"),
-    APPLICATION_JSON("application/json"),
-    APPLICATION_TAR("application/x-tar"),
-    APPLICATION_ZIP("application/zip"),
-    APPLICATION_OCTETSTREAM("application/octet-stream");
+	NOT_SET(""), TEXT_HTML("text/html"), TEXT_PLAIN("text/plain"), APPLICATION_FORM("application/x-www-form-urlencoded"), APPLICATION_JSON("application/json"), APPLICATION_TAR("application/x-tar"), APPLICATION_ZIP(
+			"application/zip"), APPLICATION_OCTETSTREAM("application/octet-stream");
 
-    private final String value;
+	private final String value;
 
-    MimeType(String value) {
-        this.value = value;
-    }
+	MimeType(String value) {
+		this.value = value;
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public String getValue() {
+		return this.value;
+	}
 
-    public static ListBoxModel getContentTypeFillItems() {
-        ListBoxModel items = new ListBoxModel();
-        for (MimeType mimeType : values()) {
-            items.add(mimeType.name());
-        }
-        return items;
-    }
+	public static ListBoxModel getContentTypeFillItems() {
+		ListBoxModel items = new ListBoxModel();
+		for (MimeType mimeType : values()) {
+			items.add(mimeType.name());
+		}
+		return items;
+	}
 
 }
