2c3ced08c86a1182e3b9cbee582da4bda3f7233a
Thad Guidry <thadguidry@gmail.com>
2017-08-30 09:51:06 -0500

    Please add support for Proxies.
    Currently we can set a System Proxy in Jenkins -> Manage Plugins -> Advanced ... but there is no way to take advantage of that with your plugin currently.

--- src/main/java/jenkins/plugins/http_request/HttpRequestExecution.java
+++ src/main/java/jenkins/plugins/http_request/HttpRequestExecution.java
@@ -125,6 +125,7 @@ public class HttpRequestExecution extends MasterToSlaveCallable<ResponseContentS
 				project, taskListener.getLogger());
 	}
 
+	//TODO: Add support for Proxy
 	private HttpRequestExecution(
 			String url, HttpMode httpMode, boolean ignoreSslErrors,
 			String body, List<HttpRequestNameValuePair> headers, Integer timeout,
