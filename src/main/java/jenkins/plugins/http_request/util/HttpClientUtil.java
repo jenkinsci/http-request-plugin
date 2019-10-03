e938b85e1d0cc537eacedbec95db44bea8a34a4f
René Scheibe <rene.scheibe@gmail.com>
2019-10-03 17:02:31 +0200

    Simplify converting parameters to string
    - use try-with-resources instead of manual stream closing
    - use IOUtils to do the copying and buffering

--- src/main/java/jenkins/plugins/http_request/util/HttpClientUtil.java
+++ src/main/java/jenkins/plugins/http_request/util/HttpClientUtil.java
@@ -1,13 +1,13 @@
 package jenkins.plugins.http_request.util;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -107,21 +107,8 @@ public class HttpClientUtil {
 	}
 
 	public static String paramsToString(List<HttpRequestNameValuePair> params) throws IOException {
-		StringBuilder sb = new StringBuilder();
-		final HttpEntity entity = toUrlEncoded(params);
-
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new InputStreamReader(entity.getContent(), StandardCharsets.UTF_8));
-			String s;
-			while ((s = br.readLine()) != null) {
-				sb.append(s);
-			}
-			return sb.toString();
-		} finally {
-			if (br != null) {
-				br.close();
-			}
+		try (InputStream is = toUrlEncoded(params).getContent()) {
+			return IOUtils.toString(is, StandardCharsets.UTF_8);
 		}
 	}
 
