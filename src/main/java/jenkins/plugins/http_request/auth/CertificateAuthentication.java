f7a543f1778054006e6149ab0da663312ef14832
Rohail Iqbal <sp20-bcs-058@students.cuisahiwal.edu.pk>
2022-10-29 14:36:32 +0000

    added reference to the exception

--- src/main/java/jenkins/plugins/http_request/auth/CertificateAuthentication.java
+++ src/main/java/jenkins/plugins/http_request/auth/CertificateAuthentication.java
@@ -36,7 +36,7 @@ public class CertificateAuthentication implements Authenticator {
 
 			return clientBuilder.build();
 		}catch(RuntimeException e){
-			throw new RuntimeException();
+			throw new RuntimeException(e);
 		} 
 		catch (Exception e) {
 			throw new IOException(e);
